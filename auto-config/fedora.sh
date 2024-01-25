d7bd6dd81d6df7ad3b47dcc14f8da574e4f7e8c7
drimerdev <miguelvicente18@aol.com>
2024-01-25 09:12:23 -0300

    Remove old dotfiles

--- auto-config/fedora.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo dnf install neovim neofetch python-pip firefox alacritty starship gnome-tweaks
-pip install pls-cli
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
-nvim
-
